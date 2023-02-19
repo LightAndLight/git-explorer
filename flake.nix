ec345f0f1a167891e2afa3f11f1d05324e1f0bb3
Isaac Elliott <isaace71295@gmail.com>
2023-02-19 13:51:06 +1000

    working ipso implementation

--- flake.nix
+++ flake.nix
@@ -1,33 +1,20 @@
 {
   inputs = {
     flake-utils.url = "github:numtide/flake-utils";
-    rust-overlay.url = "github:oxalica/rust-overlay";
+    ipso.url = "github:LightAndLight/ipso";
   };
-  outputs = { self, nixpkgs, flake-utils, rust-overlay }:
+  outputs = { self, nixpkgs, flake-utils, ipso }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs {
           inherit system;
-          overlays = [
-            rust-overlay.overlays.default
-          ];
         };
-
-        rustVersion = "1.62.1";
       
       in {
         devShell =
           pkgs.mkShell {
             buildInputs = [
-              (pkgs.rust-bin.stable.${rustVersion}.default.override {
-                extensions = [
-                  "cargo"
-                  "clippy"
-                  "rustc"
-                  "rust-src"
-                  "rustfmt"
-                ];
-              })
+              ipso.defaultPackage.${system}
             ];
           };
       }
