f880dc26e94129ebd7d688eacd9203cac0cb9964
Isaac Elliott <isaace71295@gmail.com>
2023-04-12 13:18:57 +1000

    add defaultPackage to flake

--- flake.nix
+++ flake.nix
@@ -1,9 +1,10 @@
 {
   inputs = {
     flake-utils.url = "github:numtide/flake-utils";
+    nix-filter.url = "github:numtide/nix-filter";
     ipso.url = "github:LightAndLight/ipso?tag=v0.5";
   };
-  outputs = { self, nixpkgs, flake-utils, ipso }:
+  outputs = { self, nixpkgs, flake-utils, nix-filter, ipso }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs {
@@ -17,6 +18,21 @@
               ipso.defaultPackage.${system}
             ];
           };
+        
+        defaultPackage = pkgs.stdenv.mkDerivation {
+          name = "git-explorer";
+          src = nix-filter.lib {
+            root = ./.;
+            include = [ "gitex" ];
+          };
+          buildInputs = [ ipso.defaultPackage.${system} ];
+          buildPhase = "true";
+          installPhase = ''
+            mkdir -p $out/bin
+            cp gitex $out/bin
+            chmod +x $out/bin/gitex
+          '';
+        };
       }
     );
 }
