7f0489dab4397232cf3425fbed87b7c3636394b0
Isaac Elliott <isaace71295@gmail.com>
2023-05-31 11:52:11 +1000

    use newer version of Ipso

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   inputs = {
     flake-utils.url = "github:numtide/flake-utils";
     nix-filter.url = "github:numtide/nix-filter";
-    ipso.url = "github:LightAndLight/ipso?tag=v0.5";
+    ipso.url = "github:LightAndLight/ipso?commit=e7bf506bd8f85f00c7b00b795b587f79b5bb5d9d";
   };
   outputs = { self, nixpkgs, flake-utils, nix-filter, ipso }:
     flake-utils.lib.eachDefaultSystem (system:
